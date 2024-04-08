d09a9e5c8f8502f2e3f63ef4a9aa2a616ef048c4
fijibottle <gulfurs@gmail.com>
2024-04-08 16:31:08 +0200

    snare + clap

---
+++ Modsynth3D/Assets/Scripts/UI_button_only_on.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_button_only_on : MonoBehaviour
+{
+
+    public LibPdInstance pdPatch;
+
+    // Reference to the button UI component
+    public Button button;
+
+    void Start()
+    {
+        // Subscribe to the button click event
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Function to handle button click
+    void OnClick()
+    {
+        // Send a bang to the PD patch when the button is clicked
+        pdPatch.SendBang("triggerOn");
+    }
+}
