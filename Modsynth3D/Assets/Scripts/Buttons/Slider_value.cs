0dd0217de127c41d29cd41a9136158a216ed04d1
fijibottle <gulfurs@gmail.com>
2024-04-11 21:08:37 +0200

    det f'cked up mærkeligt
    Den siger script er missing (det er det ik)
    jeg duplikere scriptet, giver nyt navn - det virker
    + masse ændre ting

---
+++ Modsynth3D/Assets/Scripts/Buttons/Slider_value.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider2Float : MonoBehaviour
+{
+    // The Pd patch we'll be communicating with.
+    public LibPdInstance pdPatch;
+    // Reference to the UI slider
+    public Slider slider;
+
+    void Update()
+    {
+        // Get the value of the UI slider
+        float sliderValue = slider.value;
+
+        // Send the slider value to the PD patch.
+        // We can then use the SendFloat() function to send our float value to
+        // that named receive object.
+        pdPatch.SendFloat("sliderValue", sliderValue);
+    }
+}
