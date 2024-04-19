cd13123c5912763ec453021fd8d7c3c0dc647b0b
fijibottle <gulfurs@gmail.com>
2024-04-19 09:46:47 +0200

    some audiokeys
    play, pause, volume

---
+++ Modsynth3D/Assets/Scripts/Buttons/AudioMan_buttons.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioMan_buttons : MonoBehaviour
+{
+    private AudioSource[] audioSource;
+    public Slider volumeSlider;
+
+    void Start(){
+
+        audioSource = FindObjectsOfType<AudioSource>();
+        volumeSlider.onValueChanged.AddListener(VolumeChange);
+
+    }
+    public void Pause(){
+
+        foreach (AudioSource source in audioSource){
+            source.Pause();
+        }
+    }
+
+     public void Play(){
+
+        foreach (AudioSource source in audioSource){
+            source.Play();
+        }
+    }
+
+    void VolumeChange(float volume){
+        
+        float scaledVolume = volume / 100f;
+
+        foreach (AudioSource source in audioSource){
+            source.volume = scaledVolume;
+        }
+    }
+}
