e78ef885f4d8d84637642e7249431b0b4842f73b
Mufasakong <Halipha.haidary@gmail.com>
2024-04-26 13:54:55 +0200

    iNTERFACEY

--- Modsynth3D/Assets/Scripts/Clicker.cs
+++ Modsynth3D/Assets/Scripts/Clicker.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Clicker : MonoBehaviour
 {
-
     public GameObject canvasMain;
     public GameObject canvasOther;
 
-
-    void OnMouseOver()
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            {
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-            // Whatever you want it to do.
-            Debug.Log("Wow");
-            canvasMain.SetActive(false);
-            canvasOther.SetActive(true);
+            // Cast a ray from mouse
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Check if the ray hit the object
+                if (hit.collider.gameObject == gameObject)
+                {
+                    // If the clicked object is this block
+                    Debug.Log("Block clicked!");
+                    
+                    // Now you can do something like showing a different canvas
+                    if (canvasMain != null)
+                        canvasMain.SetActive(false);
+                    if (canvasOther != null)
+                        canvasOther.SetActive(true);
+                }
+            }
         }
     }
-
-
-
 }
