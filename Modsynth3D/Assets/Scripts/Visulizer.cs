57198ed3f5faad1fa008f6f810576a10547ecad5
Simon <gulfurs@gmail.com>
2024-05-09 13:46:59 +0200

    av min ringefinger

--- Modsynth3D/Assets/Scripts/Visulizer.cs
+++ Modsynth3D/Assets/Scripts/Visulizer.cs
@@ -7,19 +7,21 @@ public class Visulizer : MonoBehaviour
 {
     public int numberOfSamples = 64; // Number of samples to analyze
     public Image[] visualizerBars;
-    private float maxScale = 5f; // Maximum scale of the visualizer bar
+    private float maxScale = 15f; // Maximum scale of the visualizer bar
     private float lerpSpeed = 5f;
 
     private float[] spectrumData;
     private float[] visualizerScales;
     //private AudioSource audioSource;
 
-    EffectHandler efectHandler;
+   // EffectHandler efectHandler;
 
     void Start()
     {
         spectrumData = new float[numberOfSamples];
         visualizerScales = new float[visualizerBars.Length];
+
+        SetVisualizerPositions();
 
     }
 
@@ -40,12 +42,24 @@ public class Visulizer : MonoBehaviour
             visualizerBars[i].rectTransform.pivot = new Vector2(0.5f, 0f);
 
             // Set the local position to the bottom
-            visualizerBars[i].rectTransform.localPosition = new Vector3(0f, 0f, 0f);
+            //visualizerBars[i].rectTransform.localPosition = new Vector3(xPos, 0f, 0f);
 
             // Scale only in the y-direction
             Vector3 scale = visualizerBars[i].rectTransform.localScale;
             scale.y = visualizerScales[i];
             visualizerBars[i].rectTransform.localScale = scale;
+
+            Color color = Color.Lerp(Color.blue, Color.red, visualizerScales[i] / 2);
+            visualizerBars[i].color = color;
+        }
+        
+    }
+    void SetVisualizerPositions()
+    {
+        for (int i = 0; i < visualizerBars.Length; i++)
+        {
+            float xPos = i * 60f;
+            visualizerBars[i].rectTransform.localPosition = new Vector3(xPos, 0f, 0f);
         }
     }
 }
